0fed7c23e4aa2c00558830a58fa825c22ac24ce3
Vaibhav <vaibhav.77340@gmail.com>
2022-07-18 12:21:30 +0530

    function

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,18 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
-class DishDetail extends Component{
 
-    constructor(props){
-        super(props);
-
-        this.state={
-
-        }
-
-    }
-
-    renderDish(dish){
+    function RenderDish({dish}){
         if(dish!=null){
             return(
                 <Card>
@@ -31,7 +21,7 @@ class DishDetail extends Component{
         }
     }
 
-    renderComments(dish){
+    function RenderComments({dish}){
 
 
         if(dish!=null){
@@ -67,22 +57,22 @@ class DishDetail extends Component{
 
 
 
-    render(){
+    const DishDetail = (props) =>{
 
         return(
             <div className="container">
             <div className="row">
                 <div className='col-12 col-md-5 m-1'>
-                    {this.renderDish(this.props.dish)}
+                    <RenderDish dish = {props.dish} />
                 </div>
                 <div className='col-12 col-md-5 m-1'>
-                    {this.renderComments(this.props.dish)}
+                    <RenderComments dish = {props.dish} />
                 </div>
             </div>  
             </div>   
       );
     }
 
-}
+
 
 export default DishDetail;
